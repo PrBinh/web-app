740d1c90c4d7b5a031ff67164172c89271f19f29
PrBinh <huybinh650@gmail.com>
2020-08-24 16:42:51 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-import socket
-server = socket.socket() 
-server.bind(("172.18.85.134", 5000)) 
-server.listen(4) 
-
 @app.route("/")
 def hello():
     return "Hello from Python!"
